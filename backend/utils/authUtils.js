f4b98a1f36ec8c4caa4a8b405175340b39da40fc
Feriggins <141282016+Feriggins@users.noreply.github.com>
2024-03-24 22:30:53 -0400

    Added access token function

--- backend/utils/authUtils.js
+++ backend/utils/authUtils.js
@@ -5,3 +5,18 @@ const secret = "restaurantsecretkey";
 exports.createToken = (email, id) => {
     return jwt.sign({email, id}, secret, {expiresIn: 86000})
 }
+
+exports.verifyAccessToken = (req, res, next) => {
+    const token = req.headers["access-token"];
+
+    if (!token)
+        return res.status(403).send({error: 'No access token provided'});
+
+    jwt.verify(token, secret, (err, decoded) => {
+        if (err)
+            return res.status(401).send({error: 'Un-authorized'});
+
+        req.userId = decoded.id
+        next();
+    })
+}
